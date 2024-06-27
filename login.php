f637580757c65071ba7caaacee5d3e148b3ec128
Muhammad Miftah <160307989+KeyzzCorp@users.noreply.github.com>
2024-06-27 22:36:45 +0700

    Update login.php

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - User</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles2.css">
 </head>
 <body>
     <div class="container">
